b95badf76a77755d70d0c14627a4743c7f38c9e7
Daniel Galán <mettaldanny@hotmail.com>
2021-01-02 15:53:24 -0500

    creacion de login

---
+++ SIGE/SIGE/Controllers/informationController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SIGE.Models;
+
+namespace SIGE.Controllers
+{
+    public class informationController : Controller
+    {
+        private usersEntities db = new usersEntities();
+
+        // GET: information
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.information.ToListAsync());
+        }
+
+        // GET: information/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            information information = await db.information.FindAsync(id);
+            if (information == null)
+            {
+                return HttpNotFound();
+            }
+            return View(information);
+        }
+
+        // GET: information/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: information/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "id,usuario,identificacion,nombres,apellidos,contrasena,direccion,fechaNacimiento,celular,email")] information information)
+        {
+            if (ModelState.IsValid)
+            {
+                db.information.Add(information);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(information);
+        }
+
+        // GET: information/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            information information = await db.information.FindAsync(id);
+            if (information == null)
+            {
+                return HttpNotFound();
+            }
+            return View(information);
+        }
+
+        // POST: information/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "id,usuario,identificacion,nombres,apellidos,contrasena,direccion,fechaNacimiento,celular,email")] information information)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(information).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(information);
+        }
+
+        // GET: information/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            information information = await db.information.FindAsync(id);
+            if (information == null)
+            {
+                return HttpNotFound();
+            }
+            return View(information);
+        }
+
+        // POST: information/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            information information = await db.information.FindAsync(id);
+            db.information.Remove(information);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ SIGE/SIGE/Login.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="SIGE.Models.Login" %>
+
+<!DOCTYPE html>
+
+<html  xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Acceso</title>
+    <link href="../Content/AdminLTE.css" rel="stylesheet" type="text/css" /></head>
+<body class="bg-black">
+    <div class="form-box" id="login-box">
+        <div class="header">Iniciar Sesion</div>
+        <form id="form1" runat="server">
+            <div class="body bg-gray">
+                <div class="form-group">
+                    <asp:TextBox ID="txtusuario" runat="server" CssClass="form-control" placeHolder="Ingrese usuario"></asp:TextBox>
+                </div>
+                <div class="form-group">
+                    <asp:TextBox ID="txtpassword" runat="server" CssClass="form-control" placeHolder="Ingrese contraseña"></asp:TextBox>
+                </div>
+            </div>
+            <div class="footer">
+                <asp:Button ID="btnLogin" runat="server" CssClass="btn bg-oliva" Text="Ingresar"></asp:Button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
